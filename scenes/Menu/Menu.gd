cce6223e94e47be0b7d2c482dfb4aa8b2179f18d
MILO <lautaro_milo@hotmail.com>
2024-04-21 22:06:56 -0300

    Coneccion manual (connect)
    Conectar manualmente (con .connect) la escena “Menu” con las instancias de “RedGenerator” y “VioletGenerator”. La conexión debe ser mediante la señal “button_down” y debe llamar a la función “count_new_instance”

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -8,8 +8,14 @@ func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
-	pass
-
+	
+	var generadorRojo = get_node("RedGenerator")
+	var generadorVioleta = get_node("VioletGenerator")
+	
+	generadorRojo.button_down.connect(count_new_instance)
+	generadorVioleta.button_down.connect(count_new_instance)
+	
+	
 func count_new_instance():
 	instancesCount += 1
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
